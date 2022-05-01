e73d72d21cc7031d64b1dd1899af5befa7013929
PavelKamen <kamenevpavel@bk.ru>
2022-05-01 15:21:22 +0300

    Add printiong of score table

--- main.cpp
+++ main.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
 #include <string>
+#include <fstream>
 
 #include "random_value.h"
 #include "check_value.h"
+
+ 
 
 int main(int argc, char** argv) {
 
@@ -41,6 +44,36 @@ int main(int argc, char** argv) {
 	// Цикл проверки значения на соответствие
 	int trycounter = choise_logic(true_value);
 	std::cout << " " << "trycounter " << trycounter; 
+	
+       
+		const std::string high_scores_filename = "high_scores.txt";
+		std::ifstream in_file{high_scores_filename};
+		if (!in_file.is_open()) {
+			std::cout << "Failed to open file for read: " << high_scores_filename << "!" << std::endl;
+			return -1;
+		}
+
+		std::cout << "High scores table:" << std::endl;
+
+		std::string username;
+		int high_score = 0;
+		while (true) {
+			// Read the username first
+			in_file >> username;
+			// Read the high score next
+			in_file >> high_score;
+			// Ignore the end of line symbol
+			in_file.ignore();
+
+			if (in_file.fail()) {
+				break;
+			}
+
+			// Print the information to the screen
+			std::cout << username << '\t' << high_score << std::endl;
+		}
+ 
+		
 	
 	// запись резульатов в таблицу рекордов
 	//write_to_record_table(name, trycounter);
